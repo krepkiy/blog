d6bcef7bd63dce5c0bd188ed5661ebc98bc3f0e0
krepkiy <fansity@rambler.ru>
2013-11-07 23:42:34 +0200

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Blog::Application.routes.draw do
   resources :posts
+  resources :users
+
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
