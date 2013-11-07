e8b0c9493bb439ef6e2ed975a15187daee1d2bc2
krepkiy <fansity@rambler.ru>
2013-11-07 23:51:10 +0200

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Blog::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  match '/signup',  to: 'users#new'
+  get '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
