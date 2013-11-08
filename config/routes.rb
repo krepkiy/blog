0058614cd368db58b353e107544817da72044d93
krepkiy <fansity@rambler.ru>
2013-11-08 03:52:15 +0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Blog::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
-  get "users/new"
+  get "static_pages/about"
+  #get "users/new"
 
   #root to: 'static_pages#home'
 
