955247cf3a2cc7159c406aa12f16802fc05ac214
krepkiy <fansity@rambler.ru>
2013-11-08 02:32:18 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Blog::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  match '/signup',  to: 'users#new'
+  get '/signup',  to: 'users#new'
 
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   #resources :posts
   #resources :users
 
