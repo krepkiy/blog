4e9c2ca615677ca0ff4326bce60f39971c7814d4
krepkiy <fansity@rambler.ru>
2013-11-10 02:27:34 +0200

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Blog::Application.routes.draw do
   resources :users
-  #get "users/new"
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
-  
-  get '/signup',  to: 'users#new'
 
-  #get '/home',    to: 'static_pages#home'
+  get '/signup',  to: 'users#new'
+  get '/signin',  to: 'sessions#new'
+  get '/signout', to: 'sessions#destroy', via: :delete
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
