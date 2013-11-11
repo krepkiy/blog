a272822303dc4a78d0ec5e779ee3456d87d77fea
krepkiy <fansity@rambler.ru>
2013-11-11 23:14:17 +0200

    java alert

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Blog::Application.routes.draw do
 
   get '/signup',  to: 'users#new'
   get '/signin',  to: 'sessions#new'
-  get '/signout', to: 'sessions#destroy', via: :delete
+  get '/signout', to: 'sessions#destroy', as: 'signout'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
