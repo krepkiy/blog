06ad580a4c8977f90e5b26a3c4461071b1e123e8
krepkiy <fansity@rambler.ru>
2013-11-20 21:40:50 +0200

    signout finish

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Blog::Application.routes.draw do
 
   get '/signup',  to: 'users#new'
   get '/signin',  to: 'sessions#new'
-  #get '/signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
-  get '/signout', to: 'sessions#destroy', as: 'signout'
+  match '/signout', to: 'sessions#destroy', via: :delete
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
